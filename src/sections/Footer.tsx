0870c2dbde3e89b61825548977dc0309fbbab5f5
swamimalode07 <swamimalode2004@gmail.com>
2025-03-27 02:13:34 +0530

    basic landingpage without animations

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,27 @@
-// const footerLinks = [
-//     { href: "#", label: "Contact" },
-//     { href: "#", label: "Privacy Policy" },
-//     { href: "#", label: "Terms & Conditions" },
-// ];
+import logoImage from "@/assets/images/logo.svg"
+import Image from "next/image";
 
-// export default function Footer() {
-//     return <div>Footer</div>;
-// }
+const footerLinks = [
+    { href: "#", label: "Contact" },
+    { href: "#", label: "Privacy Policy" },
+    { href: "#", label: "Terms & Conditions" },
+];
+
+export default function Footer() {
+    return <section className="py-16">
+        <div className="container">
+            <div className="flex flex-col md:flex-row  items-center md:justify-between   gap-6">
+                <div>
+                    <Image src={logoImage} alt="Logo"/>
+                </div>
+                <div>
+                    <nav className="flex gap-6">
+                        {footerLinks.map((link,index)=>(
+                            <a key={index} href={link.href} className="text-white/50 text-sm">{link.label}</a>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+}
